10def66d4be15903e05aed4f198a20d3da2d162b
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-04-03 19:16:41 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
 // Code your solution here
+
+function findMatching(arr, match) {
+    return arr.filter( et => {
+        return et.toLowerCase() === match.toLowerCase();
+    });
+}
+
+function fuzzyMatch(arr, match) {
+    return arr.filter( et => {
+        return et.substring(0,match.length) === match;
+    });
+}
+
+function matchName(arr, match) {
+    return arr.filter( et => {
+        return et.name === match;
+    });
+}
+
+// Reviewer: Please ignore the below, which was for construction
+// purposes only.
+
+// function findMatching(arr = [], match = "") {
+//     return arr.filter( et => {
+//         return et.toLowerCase() === match.toLowerCase();
+//     });
+// }
+
+// function fuzzyMatch(arr = [], match = "") {
+//     return arr.filter( et => {
+//         return et.substring(0,match.length) === match;
+//     });
+// }
+
+// function matchName(arr = [{}], match = "") {
+//     return arr.filter( et => {
+//         return et.name === match;
+//     });
+// }
+
+function testFcn() {
+    const drivers01 = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+    const drivers02 = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+    const drivers03 = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh' },
+        {
+        name: 'Sammy',
+        hometown: 'New York' } ,
+        {
+        name: 'Sally',
+        hometown: 'Cleveland' },
+        {
+        name: 'Annette',
+        hometown: 'Los Angeles' },
+        {
+        name: 'Bobby',
+        hometown: 'Tampa Bay' }
+    ];
+    match01 = 'Bobby';
+    match02 = 'Sammy';
+    match03 = 'Susan';
+    function test01(arr = [], match = "") {
+        return arr.filter( et => {
+            return et.substring(0,match.length-1) === match;
+            });
+    }
+
+    console.log(test01(drivers02, match01));
+}
+
+//testFcn();
